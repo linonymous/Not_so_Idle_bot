b16c386e08152120f3836ce5c7d9497b620f2b1a
MaheshBhosale <maheshbhosale24051996@gmail.com>
2018-07-26 19:33:02 +0530

    Added some checks to validate compare data, and removed hard bounds of selecting the list

--- Dataset/data_visualization/DataVisualizer.py
+++ Dataset/data_visualization/DataVisualizer.py
@@ -50,23 +50,28 @@ class DataVisualizer:
         if self.__x_col is None or self.__y_col is None:
             print("ERROR: Please update the values for x_col and y_col.")
             return None
-        x = self.__csv_mgr.data[self.__x_col].tolist()[0:72]
-        y = self.__csv_mgr.data[self.__y_col].tolist()[0:72]
+        if compare_data is not None:
+            if column_list is None:
+                print("ERROR: Please provide the column list to compare")
+                return None
+            if len(column_list) < 2:
+                print("ERROR: please provide columnlist with len 2 for plotting")
+        x = self.__csv_mgr.data[self.__x_col].tolist()
+        y = self.__csv_mgr.data[self.__y_col].tolist()
         if compare_data is not None and type(compare_data) is pd.DataFrame:
             # TO-DO: decide what to do with other parameter of the DF for comparison
             x_cp = compare_data[column_list[0]].tolist()
-            y_cp = compare_data[column_list[0]].tolist()
+            y_cp = compare_data[column_list[1]].tolist()
             plt.plot(y_cp, 'b')
         plt.plot(range(len(x)), y, 'r')
         plt.xticks(range(len(x)), x, rotation='vertical')
         plt.show()
-
 
 if __name__ == "__main__":
     PATH = 'C://Users//Mahesh.Bhosale//PycharmProjects//Idle_bot//Dataset//Data//IO_STATS.csv'
     csv_mgr = CSVFileManager(filename=PATH, interval=60)
     csv_mgr.get_by_interval(interval=3600)
     print(csv_mgr.data)
-    # csv_mgr.read_file()
+    csv_mgr.read_file()
     dv = DataVisualizer(csv_mgr=csv_mgr, x_col='timestamp', y_col='tps')
     dv.forecast()
