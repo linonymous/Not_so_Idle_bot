e8e47c97295db1f44dac1d2ccbd850768210229d
Swapnil.Walke <iamlinonymous@gmail.com>
2018-07-26 22:20:25 +0530

    - add cuda device code - add validation data check

--- Model/lstm_train.py
+++ Model/lstm_train.py
@@ -9,13 +9,14 @@ from torch.autograd import Variable
 
 class Seq2Seq(nn.Module):
 
-    def __init__(self, input_dim, hidden_dim=256, num_layers=2, output_dim=1):
+    def __init__(self, input_dim, hidden_dim=256, num_layers=2, output_dim=1, device='cuda'):
         super(Seq2Seq, self).__init__()
         self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.fc = nn.Linear(hidden_dim, output_dim)
+        self.device = device
 
     def forward(self, x, hc):
         x, (h, c) = self.lstm(x, hc)
@@ -25,8 +26,8 @@ class Seq2Seq(nn.Module):
 
     def init_hidden(self, n_seqs):
         weight = next(self.parameters()).data
-        return (Variable(weight.new(self.num_layers, n_seqs, self.hidden_dim).zero_()),
-                Variable(weight.new(self.num_layers, n_seqs, self.hidden_dim).zero_()))
+        return (Variable(weight.new(self.num_layers, n_seqs, self.hidden_dim).zero_()).to(self.device),
+                Variable(weight.new(self.num_layers, n_seqs, self.hidden_dim).zero_()).to(self.device))
 
 
 def get_batches(arr, n_steps, n_seqs):
@@ -54,6 +55,7 @@ if __name__ == "__main__":
     data = 100 - df["%idle"]
     data = data.as_matrix()
     val_frac = 0.1
+    device = torch.device("cuda")
 
     # divide validation and train set
     val_idx = int(len(data) * (1 - val_frac))
@@ -61,22 +63,24 @@ if __name__ == "__main__":
     val = data[val_idx:]
 
     # Initialize the class, hidden_state, criteria and optimizer
-    seq = Seq2Seq(input_dim=1, hidden_dim=256, num_layers=2, output_dim=1)
-    criterion = torch.nn.MSELoss(size_average=False)
-    optimizer = torch.optim.Adam(seq.parameters(), lr=0.0001)
-    epoch = 10
+    seq = Seq2Seq(input_dim=1, hidden_dim=256, num_layers=2, output_dim=1, device=device).to(device)
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(seq.parameters(), lr=0.01)
+    epoch = 81
     count = 1
     n_seqs = 5
-    n_steps = 100
-    a=0
+    n_steps = 500
+    x_ = None
+    y_ = None
     for _ in range(epoch):
         h = seq.init_hidden(n_seqs=n_seqs)
+        train_losses = []
         for x, y in get_batches(df_data, n_seqs=n_seqs, n_steps=n_steps):
             x = x.reshape((*x.shape, 1))
-            x = Variable(torch.from_numpy(x).float())
+            x = Variable(torch.from_numpy(x).float()).to(device)
             y = y.reshape((*y.shape, 1))
-            y = Variable(torch.from_numpy(y).float())
-            h = tuple([Variable(each.data) for each in h])
+            y = Variable(torch.from_numpy(y).float()).to(device)
+            h = tuple([Variable(each.data).to(device) for each in h])
             x, h = seq.forward(x, hc=h)
             loss = criterion(x.view(n_seqs, n_steps, 1), y)
             optimizer.zero_grad()
@@ -84,18 +88,32 @@ if __name__ == "__main__":
             # optimizer.zero_grad()
             # nn.utils.clip_grad_norm_(seq.parameters(), 5)
             optimizer.step()
-            if _ % count == 0:
-                val_h = seq.init_hidden(n_seqs=n_seqs)
-                val_losses = []
-                for b, c in get_batches(val, n_seqs=n_seqs, n_steps=n_steps):
-                    b = b.reshape((*b.shape, 1))
-                    input = Variable(torch.from_numpy(b).float())
-                    c = c.reshape((*c.shape, 1))
-                    c = Variable(torch.from_numpy(c).float())
-                    output, val_h = seq.forward(input, val_h)
-                    val_loss = criterion(output.view(n_seqs, n_steps, 1), c)
-                    val_losses.append(val_loss.data[0])
-                print("Epoch: {}/{}...".format(_ + 1, epoch),
-                      "Step: {}...".format(count),
-                      "Loss: {:.4f}...".format(loss.data[0]),
-                      "Val Loss: {:.4f}".format(np.mean(val_losses)))
+            train_losses.append(loss.data[0])
+        val_h = seq.init_hidden(n_seqs=n_seqs)
+        val_losses = []
+        for b, c in get_batches(val, n_seqs=n_seqs, n_steps=n_steps):
+                b = b.reshape((*b.shape, 1))
+                input = Variable(torch.from_numpy(b).float()).to(device)
+                c = c.reshape((*c.shape, 1))
+                c = Variable(torch.from_numpy(c).float()).to(device)
+                output, val_h = seq.forward(input, val_h)
+                val_loss = criterion(output.view(n_seqs, n_steps, 1), c)
+                val_losses.append(val_loss.data[0])
+        print("Epoch: {}/{}...".format(_ + 1, epoch),
+              "Loss: {:.4f}...".format(np.mean(train_losses)),
+              "Val Loss: {:.4f}".format(np.mean(val_losses)))
+    predicted = []
+    target = []
+    for i in val:
+        a = np.array(i)
+        input = Variable(torch.from_numpy(a).float()).to(device)
+        h = seq.init_hidden(n_seqs=1)
+        x, (h, c) = seq.forward(input.view((1, 1, 1)), h)
+        k = x.data.cpu().numpy().reshape(1).tolist()[0]
+        l = input.data.cpu().numpy().tolist()
+        predicted.append(k)
+        target.append(l)
+    df1 = pd.DataFrame(data=predicted, index=None, columns=None)
+    df2 = pd.DataFrame(data=target, index=None, columns=None)
+    result = pd.concat([df1, df2], axis=1)
+    result.to_csv('abc.csv', encoding='utf-8', header=False, index=False)
